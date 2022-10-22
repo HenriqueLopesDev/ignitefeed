3e6cf5f3da0179d8735f6372177d87596902abe3
Henrique Lopes <henriquebonfanti2018@gmail.com>
2022-10-22 15:34:17 -0300

    Add the comment component to the post component.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post(){
@@ -6,7 +7,7 @@ export function Post(){
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://img.freepik.com/premium-vector/anime-boy-avatar_24640-79424.jpg" />
-                    <div className={styles.authorinfo}>
+                    <div className={styles.authorInfo}>
                         <strong>Henrique Lopes</strong>
                         <span>Web Developer</span>
                     </div>
@@ -18,9 +19,30 @@ export function Post(){
 
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
+            <p><a href="">jane.design/doctorcare</a></p>
 
-            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            <p>
+                <a href="">#novoprojeto</a>{' '}
+                <a href="">#nlw</a>{' '}
+                <a href="">#rocketseat</a>
+            </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+                
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
