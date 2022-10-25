92d6575dae989a94a20a1978a97fcd214b0d50bf
Henrique Lopes <henriquebonfanti2018@gmail.com>
2022-10-25 09:41:35 -0300

    Create the like functionality.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }){
 
+    const[likeCount, setLikeCount] = useState(0)
+
     function handleDeleteComment(){
         onDeleteComment(content)
+    }
+
+    function handleLikeComment(){
+        setLikeCount( (state) => {
+            return state + 1
+        })
     }
 
     return(
@@ -30,9 +40,9 @@ export function Comment({ content, onDeleteComment }){
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
                 
